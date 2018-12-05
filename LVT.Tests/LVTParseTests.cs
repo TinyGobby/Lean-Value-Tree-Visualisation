36722923017293c7cb3cac0a49bab1e11b83c70a
Patil <PATILS3@legal.regn.net>
2018-12-05 12:18:00 +0000

    refactoring tests

--- LVT.Tests/LVTParseTests.cs
+++ LVT.Tests/LVTParseTests.cs
@@ -1,11 +1,9 @@
 ﻿using LVT.LVT.Services;
 using NUnit.Framework;
 using System.IO;
-// TODO: tests for typeof not needed
-// TODO: amend tests to use stream instead of absolute path
+// TODO: tests for typeof in strongly typed language are needed? : Ask Kieron/Dan for review
 namespace LVT.Tests
 {
-
     [TestFixture]
     public class ParseJSONLVTFromStreamTest
     {
@@ -22,6 +20,7 @@ namespace LVT.Tests
             Assert.AreNotEqual(null, TestSingleBranchLVT.Vision, "ParseJsonLVT should populate objects inside the LVT");
         }
     }
+
     [TestFixture]
     public class LVTObjectTests
     {
@@ -55,7 +54,6 @@ namespace LVT.Tests
             Assert.AreEqual("goalTitle", GoalObj.Title, "Test Goal should have title");
             Assert.AreEqual(1, GoalObj.Bets.Count, "Test Goal should have one Bet");
         }
-
 
         [Test]
         public void SingleBranchLVT_Bet()
@@ -100,155 +98,4 @@ namespace LVT.Tests
             Assert.AreEqual("epicDeadline", EpicObj.Deadline, "Test Epic has a deadline");
         }
     }
-
-    [TestFixture]
-    public class LVTVisionObjectTests
-    {
-        LeanValueTree SingleBranchLVT;
-
-        [SetUp]
-        public void Setup()
-        {
-            Stream stream = new MemoryStream(Properties.Resources.TestLVT);
-            StreamReader streamReader = new StreamReader(stream);
-            JsonParser Parser = new JsonParser();
-            SingleBranchLVT = Parser.ParseJsonLVTFromStream(streamReader);       
-        }
-
-        //[Test]
-        //public void SingleBranchLVT_Vision_Tests()
-        //{
-        //    Vision VisionObj = SingleBranchLVT.Vision;
-
-        //    Assert.AreEqual("visionTitle", VisionObj.Title, "Vision should have title");
-        //    Assert.IsInstanceOf(typeof(Vision), VisionObj, "Test Vision should be instance of Vision Class");
-        //    Assert.AreEqual(1, VisionObj.Goals.Count, "Test Vision should have one goal");
-        //}
-    }
-
-    [TestFixture]
-    public class LVTGoalObjectTests
-    {
-        LeanValueTree SingleBranchLVT;
-
-        [SetUp]
-        public void Setup()
-        {
-            Stream stream = new MemoryStream(Properties.Resources.TestLVT);
-            StreamReader streamReader = new StreamReader(stream);
-            JsonParser Parser = new JsonParser();
-            SingleBranchLVT = Parser.ParseJsonLVTFromStream(streamReader);
-        }
-
-        //[Test]
-        //public void SingleBranchLVT_Goal()
-        //{
-        //    Goal GoalObj = SingleBranchLVT.Vision.Goals[0];
-
-        //    Assert.IsInstanceOf(typeof(Goal), GoalObj,"Test Goal should be instance of Goal class");
-        //    Assert.AreEqual("goalTitle", GoalObj.Title, "Test Goal should have title");
-        //    Assert.AreEqual(1, GoalObj.Bets.Count, "Test Goal should have one Bet");
-        //}
-    }
-
-    [TestFixture]
-    public class LVTBetObjectTests
-    {
-        LeanValueTree SingleBranchLVT;
-
-        [SetUp]
-        public void Setup()
-        {
-            Stream stream = new MemoryStream(Properties.Resources.TestLVT);
-            StreamReader streamReader = new StreamReader(stream);
-            JsonParser Parser = new JsonParser();
-            SingleBranchLVT = Parser.ParseJsonLVTFromStream(streamReader);
-        }
-
-        //[Test]
-        //public void SingleBranchLVT_Bet()
-        //{
-        //    Bet BetObj = SingleBranchLVT.Vision.Goals[0].Bets[0];
-
-        //    Assert.IsInstanceOf(typeof(Bet), BetObj, "Test Bet should be instance of Bet class");
-        //    Assert.AreEqual("betTitle", BetObj.Title, "Test Bet should have a title");
-        //    Assert.AreEqual(1, BetObj.Initiatives.Count, "Test Bet should have one Initiative");
-        //}
-    }
-
-    [TestFixture]
-    public class TestsLVTInitiativeObject
-    {
-        LeanValueTree SingleBranchLVT;
-
-        [SetUp]
-        public void Setup()
-        {
-            Stream stream = new MemoryStream(Properties.Resources.TestLVT);
-            StreamReader streamReader = new StreamReader(stream);
-            JsonParser Parser = new JsonParser();
-            SingleBranchLVT = Parser.ParseJsonLVTFromStream(streamReader);
-        }
-        //[Test]
-        //public void SingleBranchLVT_Initiative()
-        //{
-        //    Initiative InitiativeObj = SingleBranchLVT.Vision.Goals[0].Bets[0].Initiatives[0];
-
-        //    Assert.IsInstanceOf(typeof(Initiative), InitiativeObj, "Test Initiative should be instance of Iniative class");
-        //    Assert.AreEqual("initiativeTitle", InitiativeObj.Title, "Test Initiative should have title");
-        //    Assert.AreEqual(1, InitiativeObj.Measures.Count, "Test Initiative should have one Measure");
-        //    Assert.AreEqual(1, InitiativeObj.Epics.Count, "Test Initiative should have one Epic");
-        //}
-    }
-
-    [TestFixture]
-    public class TestsLVTMeasureObj
-    {
-        LeanValueTree SingleBranchLVT;
-
-        [SetUp]
-        public void Setup()
-        {
-            Stream stream = new MemoryStream(Properties.Resources.TestLVT);
-            StreamReader streamReader = new StreamReader(stream);
-            JsonParser Parser = new JsonParser();
-            SingleBranchLVT = Parser.ParseJsonLVTFromStream(streamReader);
-        }
-        //[Test]
-        //public void SingleBranchLVT_Measure()
-        //{
-        //    Measure MeasureObj = SingleBranchLVT.Vision.Goals[0].Bets[0].Initiatives[0].Measures[0];
-
-        //    Assert.IsInstanceOf(typeof(Measure), MeasureObj, "Test Measure is instance of Measure class");
-        //    Assert.AreEqual("measureDescription", MeasureObj.Description, "Test Measure has a description");
-        //    Assert.AreEqual("measureDeadline", MeasureObj.Deadline, "Test Measure has a deadline");
-        //    Assert.AreEqual(1, MeasureObj.Amount, "Test Measure has an amount");
-        //    Assert.AreEqual("measureUnits", MeasureObj.Units, "Test Measure has units");
-        //}
-    }
-
-    [TestFixture]
-    public class TestsLVTEpicObj
-    {
-        LeanValueTree SingleBranchLVT;
-
-        [SetUp]
-        public void Setup()
-        {
-            Stream stream = new MemoryStream(Properties.Resources.TestLVT);
-            StreamReader streamReader = new StreamReader(stream);
-            JsonParser Parser = new JsonParser();
-            SingleBranchLVT = Parser.ParseJsonLVTFromStream(streamReader);
-        }
-
-        //[Test]
-        //public void SingleBranchLVT_Epic()
-        //{
-        //    Epic EpicObj = SingleBranchLVT.Vision.Goals[0].Bets[0].Initiatives[0].Epics[0];
-
-        //    Assert.IsInstanceOf(typeof(Epic), EpicObj, "Test Epic is instance of Epic class");
-        //    Assert.AreEqual("epicDescription", EpicObj.Description, "Test Epic has a description");
-        //    Assert.AreEqual("epicDeadline", EpicObj.Deadline, "Test Epic has a deadline");
-        //}
-    }  
 }
