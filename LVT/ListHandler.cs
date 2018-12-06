69a89f8e1484b7a8839fda30c1701540f58b60a7
Patrick Harris <HARRISPN@legal.regn.net>
2018-12-06 14:35:35 +0000

    Refactored to send Nodes with ID information instead of IDs

---
+++ LVT/ListHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LVT
+{
+    public class ListHandler
+    {
+        public List<Node> NodeList { get; set; }
+        public List<List<Node>> EdgeList { get; set; }
+        public Node PreviousNode { get; set; }
+    }
+}
