536d49b6ff2039e5a6e4cbd9ceebcb3a8b5fef2f
Patil <PATILS3@legal.regn.net>
2018-12-07 12:15:58 +0000

    adds file input from command Line & checks for empty input in working process

--- LVT/Program.cs
+++ LVT/Program.cs
@@ -1,5 +1,4 @@
-﻿using CommandLine;
-using LVT.LVT.Services;
+﻿using LVT.LVT.Services;
 using System;
 using System.IO;
 
@@ -7,44 +6,20 @@ namespace LVT
 {
     public class Program
     {
-        public class Options
-        {
-            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
-            public bool Verbose { get; set; }
-        }
 
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args)
-                   .WithParsed<Options>(o =>
-                   {
-                       if (o.Verbose)
-                       {
-                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
-                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
-                       }
-                       else
-                       {
-                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
-                           Console.WriteLine("Quick Start Example!");
-                       }
-                   });
+            Console.WriteLine(args[0]);
+            //this takes an absolute path and needs to be adapted for each computer depending on where the file lives that you want to open - but this allows you to pass different template trees.
+            //see Tests for an alternative way that uses project resources instead - so tests can be run no matter on whose environment
+            StreamReader file = File.OpenText(args[0]);
+            JsonParser Parser = new JsonParser();
+            LeanValueTree newTree = Parser.ParseJsonLVTFromStream(file);
+            //this is an example of what is returned when you access the object:
+            Console.WriteLine(newTree);
+            Console.WriteLine(newTree.Vision.Title);
+            Console.WriteLine(newTree.Vision.Goals[0]);
+            Console.WriteLine(newTree.Vision.Goals[0].Bets[0]);
         }
-
-
-        //static void Main(string[] args)
-        //{
-        //    CommandLineParser parser = new CommandLineParser();
-        //    //this takes an absolute path and needs to be adapted for each computer depending on where the file lives that you want to open - but this allows you to pass different template trees.
-        //    //see Tests for an alternative way that uses project resources instead - so tests can be run no matter on whose environment
-        //    StreamReader file = File.OpenText(args[0]);
-        //    JsonParser Parser = new JsonParser();
-        //    LeanValueTree newTree = Parser.ParseJsonLVTFromStream(file);
-        //    //this is an example of what is returned when you access the object:
-        //    Console.WriteLine(newTree);
-        //    Console.WriteLine(newTree.Vision.Title);
-        //    Console.WriteLine(newTree.Vision.Goals[0]);
-        //    Console.WriteLine(newTree.Vision.Goals[0].Bets[0]);
-        //}
     }
 }
