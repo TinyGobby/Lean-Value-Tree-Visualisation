d4edbac8117ecb679a2da1f2937edf8be6db916b
victorjefferies <victor.jefferies@lexisnexis.co.uk>
2018-12-07 10:47:15 +0000

    Refactored RecursiveTreeCrawler to include UpdateListHandler and removed BottomOfTree.

--- LVT/Node.cs
+++ LVT/Node.cs
@@ -20,11 +20,6 @@ namespace LVT
 
         public List<Node> InternalNodeList { get; set; }
 
-        public bool IsNotBottomOfTree()
-        {
-            return InternalNodeList.Count != 0;
-        }
-
         public string GenerateNodeID(List<Node> nodeList)
         {
             int count = 0;
@@ -55,14 +50,21 @@ namespace LVT
             return edgeList;
         }
 
-        public ListHandler RecursiveTreeCrawler(ListHandler listHandler)
+
+        public ListHandler UpdateListHandler(ListHandler listHandler)
         {
-            // Returns an object with a list of all nodes and all edges between nodes
             GenerateNodeID(listHandler.NodeList);
             listHandler.EdgeList = UpdateEdgeList(listHandler.EdgeList, listHandler.PreviousNode);
             listHandler.PreviousNode = this;
             listHandler.NodeList.Add(this);
-            
+
+            return listHandler;
+        }
+
+        public ListHandler RecursiveTreeCrawler(ListHandler listHandler)
+        {
+             listHandler = UpdateListHandler(listHandler);
+
                 foreach (Node node in InternalNodeList)
                 {
                     listHandler = node.RecursiveTreeCrawler(listHandler);
