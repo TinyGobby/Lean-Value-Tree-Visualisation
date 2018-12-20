6a823445596f9958edaeae7c2f1c5cc883ec0426
beckerfs <freya.becker@lexisnexis.co.uk>
2018-12-20 17:38:19 +0000

    Adds BetPresenterTest 'VisualiseToString_Bet_NoInitiatives'

--- LVT.Tests/BetPresenterTests.cs
+++ LVT.Tests/BetPresenterTests.cs
@@ -9,50 +9,43 @@ namespace LVT.Tests
 {
     class BetPresenterTests
     {
-        public Bet _testBet;
-        public BetPresenter _TBP;
-        public Initiative _testInitiative;
-        public Mock<IInitiativePresenter> _MIP;
+        private Bet _testBet;
+        private BetPresenter _TBP;
+        private Initiative _testInitiative;
+        private Mock<IInitiativePresenter> _MIP;
+        private string _ParentNodeID;
 
         [SetUp]
         public void SetupForTest()
         {
-            _TBP = new BetPresenter();
             _testBet = new Bet("Test Bet Title");
+            _ParentNodeID = "Parent Goal Node Test ID";
             _testInitiative = new Initiative("Test Initiative Title");
+            System.Console.WriteLine(_testInitiative);
             _MIP = new Mock<IInitiativePresenter>();
         }
-        //[Test]
-        //TestBetVisualizeToStringOneInitiative()
-        //{
-        //    string title = "betTitle";
-        //    var mockInitiative = new Mock<Initiative>();
-        //    mockInitiative.Setup(foo => foo.ProcessInitiatives).Returns(;
-        //    BetPresenter BP = new BetPresenter(mockInitiative);
-        //    Bet testBet = new Bet(title);
-        //    string parentNode = "hiIAmAParentNode";
-        //    string actual = BP.VisualizeToString(testBet, parentNode);
-        //    string BetID = testBet.NodeID;
-
-        //    string expected = "[{ v: '" + BetID + "', f: 'Bet" + "<div style=\"font-style:italic\">" + title + "</div>'}, " + $"'{parentNode}']";
-
-        //    Assert.AreEqual(expected, actual);
-        //    Assert.verify (or similar, to verify call is made to method)
-
-        //mock_ip.Setup(foo => foo.ProcessInitiatives).Returns("SomeSTring");
-
-        //}
-
         [Test]
-        public void VisualiseToString_Bet_WithOneInitiative()
+        public void VisualiseToString_Bet_NoInitiatives()
         {
-            _testBet.Initiatives.Add(_testInitiative);
-            _MIP.Setup(mip => mip.VisualizeToString(_testInitiative, "Parent Node Test ID")).Returns("Test Initiative One of One Title");
-            
-            string result = _TBP.VisualizeToString(_testBet, _testBet.NodeID);
-            string expected = "[{ v: '" + _testInitiative.NodeID + "', f: 'Bet" + "<div style=\"font-style:italic\">" + _testBet.Title + "</div>'}, " + $"'{_testBet.NodeID}']" + "Test Initiative One of One Title";
+            _TBP = new BetPresenter();
+
+            string result = _TBP.VisualizeToString(_testBet, _ParentNodeID);
+            string expected = "[{ v: '" + _testBet.NodeID + "', f: 'Bet" + "<div style=\"font-style:italic\">" + _testBet.Title + "</div>'}, " + $"'{_ParentNodeID}']";
 
             Assert.AreEqual(expected, result);
-      }
+        }
+
+        //[Test]
+        //public void VisualiseToString_Bet_WithOneInitiative()
+        //{
+        //    _testBet.Initiatives.Add(_testInitiative);
+        //    _MIP.Setup(mip => mip.VisualizeToString(_testInitiative, "parent bet node")).Returns("This initiative presenter method has been mocked");
+        //    _TBP = new BetPresenter(_MIP.Object);
+
+        //    string result = _TBP.VisualizeToString(_testBet, _ParentNodeID);
+        //    string expected = "[{ v: '" + _testBet.NodeID + "', f: 'Bet" + "<div style=\"font-style:italic\">" + _testBet.Title + "</div>'}, " + $"'{_ParentNodeID}']" + "Test Initiative One of One Title";
+
+        //    Assert.AreEqual(expected, result);
+        //}
     }
 }
