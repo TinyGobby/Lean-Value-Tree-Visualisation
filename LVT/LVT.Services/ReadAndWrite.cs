8b118dc53091df5eadc76f42795916befc868645
beckerfs <freya.becker@lexisnexis.co.uk>
2019-01-03 13:27:04 +0000

    moves read and write from Program.cs to separate class

---
+++ LVT/LVT.Services/ReadAndWrite.cs
@@ -0,0 +1,44 @@
+﻿using LVT.LVT.Services;
+using System;
+using System.IO;
+
+namespace LVT
+{
+    public class ReadAndWrite
+    {
+        public static void CheckCommandLineArgument(String[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide the location of the JSON file");
+            }
+            else
+            {
+                Console.WriteLine(args[0]);
+                StreamReader file = File.OpenText(args[0]);
+            }
+        }
+
+        public static LeanValueTree ParseLVTData(StreamReader file)
+        {
+            JsonParser Parser = new JsonParser();
+            return Parser.ParseJsonLVTFromStream(file);
+        }
+
+        public static void CreateLVTHtml(LeanValueTree tree)
+        {
+            VisionPresenter VP = new VisionPresenter();
+
+            Directory.CreateDirectory("C:\\temp");
+            string OrgaChart = VP.VisualizeToString(tree.Vision);
+            SaveToDisk(OrgaChart);
+        }
+
+        public static void SaveToDisk(string chartdata)
+        {
+            string content = Properties.Resources.TemplateHTMLHeader + chartdata + Properties.Resources.TemplateHTMLFooter;
+            File.WriteAllText(@"C:\temp\LVT.html", content);
+            Console.WriteLine(@"Your LeanValueTree has been saved to C:\temp\LVT.html");
+        }
+    }
+}
--- LVT/Program.cs
+++ LVT/Program.cs
@@ -1,6 +1,4 @@
-﻿using LVT.LVT.Services;
-using System;
-using System.IO;
+﻿using System.IO;
 
 namespace LVT
 {
@@ -12,16 +10,13 @@ namespace LVT
             RunFromCommandLine(args);
         }
 
-        // the below should probably be moved to services
-      
         public static void RunFromCommandLine(string[] args)
         {
-            CommandLineArg argsList = new CommandLineArg();
-            argsList.CheckCommandLineArgument(args);
+            ReadAndWrite.CheckCommandLineArgument(args);
           
             StreamReader file = File.OpenText(args[0]);
-            LeanValueTree newTree = ParseLVTData(file);
-            CreateLVTHtml(newTree);
+            LeanValueTree newTree = ReadAndWrite.ParseLVTData(file);
+            ReadAndWrite.CreateLVTHtml(newTree);
         }
 
         public static void RunFromSolution()
@@ -29,30 +24,8 @@ namespace LVT
             //this takes an absolute path and needs to be adapted for each computer depending on where the file lives that you want to open - but this allows you to pass different template trees.
             //StreamReader file = File.OpenText(@"C:\Users\beckerfs\Documents\Projects\LVT\Lean-Value-Tree-Visualisation\LVT\SingleBranchLVT.json");
             StreamReader file = File.OpenText(@"C:\Users\beckerfs\Documents\Projects\LVT\Lean-Value-Tree-Visualisation\LVT\TwoGoalsTwoBetsLVT.json");
-            LeanValueTree newTree = ParseLVTData(file);
-            CreateLVTHtml(newTree);
-        }
-
-        public static LeanValueTree ParseLVTData(StreamReader file)
-        {
-            JsonParser Parser = new JsonParser();
-            return Parser.ParseJsonLVTFromStream(file);
-        }
-
-        public static void CreateLVTHtml(LeanValueTree tree)
-        { 
-            VisionPresenter VP = new VisionPresenter();
-
-            Directory.CreateDirectory("C:\\temp");
-            string OrgaChart = VP.VisualizeToString(tree.Vision);
-            SaveToDisk(OrgaChart);
-        }
-
-        public static void SaveToDisk(string chartdata)
-        {
-            string content = Properties.Resources.TemplateHTMLHeader + chartdata + Properties.Resources.TemplateHTMLFooter;
-            File.WriteAllText(@"C:\temp\LVT.html", content);
-            Console.WriteLine(@"Your tree has been saved to C:\temp\LVT.html");
+            LeanValueTree newTree = ReadAndWrite.ParseLVTData(file);
+            ReadAndWrite.CreateLVTHtml(newTree);
         }
     }
 }
