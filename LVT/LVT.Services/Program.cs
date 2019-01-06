d2ea59ff82ee86209306e4905a67069a503e5bc7
beckerfs <freya.becker@lexisnexis.co.uk>
2019-01-06 19:23:09 +0000

    adds basic web app implementation

--- LVT/LVT.Services/Program.cs
+++ LVT/LVT.Services/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System;
+using LVT.LVT.Services;
 
 namespace LVT
 {
@@ -13,24 +14,27 @@ namespace LVT
 
         public static void RunFromCommandLine(string[] args)
         {
-            if (ReadAndWrite.IsValidArgument(args))
+            if (Validate.IsValidArgument(args))
             {
                 Console.WriteLine($"Processing {args[0]}");
                 StreamReader file = File.OpenText(args[0]);
-                ReadAndWrite.BuildTree(file);
+                string LVT = ReadAndWrite.BuildTree(file);
+                string FullHTML = ReadAndWrite.CreateFullHTML(LVT);
+                ReadAndWrite.SaveToDisk(FullHTML);
             }
             else
             {
-                ReadAndWrite.ShowErrorMessage(args);
+                Validate.ShowErrorMessage(args);
             }
         }
 
         public static void RunFromSolution()
         {
             //This takes an absolute path. Copy the full path and file name of your .json file (for example: "C:\temp\Test.json")
-            //StreamReader file = File.OpenText(@"C:\Users\beckerfs\Documents\Projects\LVT\Lean-Value-Tree-Visualisation\LVT\SingleBranchLVT.json");
-            StreamReader file = File.OpenText(@"C:\Users\beckerfs\Documents\Projects\LVT\Lean-Value-Tree-Visualisation\LVT\TwoGoalsTwoBetsLVT.json");
-            ReadAndWrite.BuildTree(file);
+            StreamReader jsondata = new StreamReader(@"C:\Users\beckerfs\Documents\Projects\LVTWebApp\Lean-Value-Tree-Visualisation\LVT\SingleBranchLVT.json");
+            string LVT = ReadAndWrite.BuildTree(jsondata);
+            string FullHTML = ReadAndWrite.CreateFullHTML(LVT);
+            ReadAndWrite.SaveToDisk(FullHTML);
         }
     }
 }
---
+++ LVT/LVT.Services/Validate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LVT.LVT.Services
+{
+    public class Validate
+    {
+        internal static bool IsValidArgument(string[] args)
+        {
+            return args.Length == 1 && args[0].ToLower().EndsWith(".json");
+        }
+
+        internal static void ShowErrorMessage(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide the location of the JSON file you want to parse");
+            }
+            else if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("This file does not exist. Please provide valid path and filename.");
+            }
+            else if (!args[0].ToLower().EndsWith(".json"))
+            {
+                Console.WriteLine("File must be in JSON format");
+            }
+        }
+
+
+
+        //public static bool IsValidFilename(string filename)
+        //{
+        //    return filename != null && filename.ToLower().EndsWith(".json");
+        //}
+
+        //public static string ShowErrorMessageWeb(string filename)
+        //{
+        //    string message;
+
+        //    if (filename == null)
+        //    {
+        //        message = "Please select a JSON file";
+        //    }
+        //    else if (!filename.ToLower().EndsWith(".json"))
+        //    {
+        //        message = "File must be in JSON format";
+        //    }
+        //    return message;
+    //    }
+    }
+}
